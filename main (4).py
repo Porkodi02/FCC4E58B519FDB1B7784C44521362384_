d620326f60c30579253801d2d113320ae02447c2
Porkodi02 <144463526+Porkodi02@users.noreply.github.com>
2023-09-27 20:46:12 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,49 +1,16 @@
-class BankAccount:
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-    else:
-      print("Invalid deposit amount. Amount must be greater than 0.")
-
-  def withdraw(self, amount):
-    if amount > 0:
-      if amount <= self.__account_balance:
-        self.__account_balance -= amount
-        print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-      else:
-        print("Insufficient funds.")
-    else:
-      print("Invalid withdrawal amount. Amount must be greater than 0.")
-
-  def display_balance(self):
-    print(
-        f"Account balance for {self.__account_holder_name} (Account #{self.__account_number}): ${self.__account_balance}"
-    )
+  return indices
 
 
-# Testing the BankAccount class
-if __name__ == "__main__":
-  # Create a bank account
-  account = BankAccount("12345", "John Doe", 1000.0)
-
-  # Display initial balance
-  account.display_balance()
-
-  # Make a deposit
-  account.deposit(500.0)
-
-  # Make a withdrawal
-  account.withdraw(200.0)
-
-  # Try to withdraw more than the balance
-  account.withdraw(1500.0)
-
-  # Display the updated balance
-  account.display_balance()
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
